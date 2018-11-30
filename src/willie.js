8c92cec2c08f2ad3498152dadac7822d6d35740b
Josh <mdx.programs@gmail.com>
2018-11-29 22:37:05 -0500

    adding better parsing for frontmatter and pages

--- src/willie.js
+++ src/willie.js
@@ -1,17 +1,12 @@
-const fs = require("fs");
-const path = require('path');
-const Handlebars = require("handlebars");
+const fs = require("fs-extra");
+const path = require("path");
+const handlebars = require("handlebars");
+const fm = require("front-matter");
 
-/**
- * Should enable ability to create "controllers" for layouts
- * This would handle extra logic
- * Should give option for custom handlebars functions
- * includes needed as well
- */
+// site config
+const config = require("./config.json");
 
-// read config yaml
-
-// run through pages with templates
+// walkSync - recursive file finder with relative path
 const walkSync = (dir, filelist = []) =>
   fs
     .readdirSync(dir)
@@ -21,12 +16,46 @@ const walkSync = (dir, filelist = []) =>
         : filelist.concat(path.join(dir, file))[0]
     );
 
-const templateFiles = walkSync("src/templates");
+// get list of files
 const pageFiles = walkSync("src/pages");
+const templateFiles = walkSync("src/templates");
 
-const template = Handlebars.compile(
-  fs.readFileSync("src/templates/layout.html", "utf8")
-);
+// clean dist/ first
+fs.emptyDir("dist").then(() => {
+  pageFiles.forEach(pFile => {
+    const { attributes, body } = fm(fs.readFileSync(pFile, "utf8"));
 
-// write pages
-fs.writeFileSync("dist/index.html", template({ name: "Josh" }), "utf8");
+    // layout is required per page
+    if (!attributes.hasOwnProperty("layout")) {
+      return console.error(`File: ${pFile} must include a layout property`);
+    }
+
+    const layoutTemp = handlebars.compile(
+      fs.readFileSync(`src/templates/layouts/${attributes.layout}.html`, "utf8")
+    );
+
+    // if permalink specified, use it
+    const writePath = attributes.hasOwnProperty("permalink")
+      ? attributes.permalink
+      : pFile.replace("src/pages/", "").replace(".html", "");
+
+    // no need for extra directory for index.html
+    if (writePath !== "index") {
+      fs.ensureDir(`dist/${writePath}`)
+        .then(() => {
+          fs.writeFileSync(
+            `dist/${writePath}/index.html`,
+            layoutTemp({ site: config, page: attributes, content: body }),
+            "utf8"
+          );
+        })
+        .catch(err => console.error(err));
+    } else {
+      fs.writeFileSync(
+        `dist/index.html`,
+        layoutTemp({ site: config, page: attributes, content: body }),
+        "utf8"
+      );
+    }
+  });
+});
